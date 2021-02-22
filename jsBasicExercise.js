ee3333bbfe162a026c4ec5ee0f699afb8238c39f
hcampbell1212 <heather.campbell1212@gmail.com>
2021-02-22 12:51:09 -0600

    JSBasicsExercise q's 1-8

---
+++ jsBasicExercise.js
@@ -0,0 +1,41 @@
+// "1"
+console.log(1);
+
+//  "2"
+//  undefined
+//  null
+//  boolean
+//  string
+//  number
+
+// "3"
+//  Parentheses
+//  Exponentiation
+//  Multiplication
+//  Division
+//  Modulus
+//  Addition
+// Substraction
+
+//  "4"
+console.log(5%4);
+console.log (5%[2*2]);
+
+//   "5"
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+//  "6"
+console.log("Wanda" + "Vision");
+console.log("Wanda "+"Vision");
+console.log("Wanda" + " " + "Vision");
+
+// "7"
+console.log("Heather".length + "Campbell".length);
+
+//  "8"
+console.log("Avengers: Endgame"[2]);
+
+
+
+
+
